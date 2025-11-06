6696f29e2946079f7b56567e9bf3c84c8ca1b8e4
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-06 08:55:13 +0100

    Style page

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,19 @@
+
+export const Dashboard = () => {
+  return (
+    <>
+      <header className="relative shadow-sm" id="dashboard">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Dashboard
+          </h1>
+        </div>
+      </header>
+      <main className="h-256">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+        </div>
+      </main>
+    </>
+  );
+};
---
+++ src/components/Team.tsx
@@ -0,0 +1,19 @@
+
+export const Team = () => {
+  return (
+    <>
+      <header className="relative shadow-sm" id="dashboard">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Team
+          </h1>
+        </div>
+      </header>
+      <main className="h-256">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+        </div>
+      </main>
+    </>
+  );
+};
