8991b0d2814a64899ad7f272fa28cb5b718f4bbc
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-06 08:11:33 +0100

    Initialize project + base template

---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+import 'react';
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    command?: string;
+    commandFor?: string;
+    anchor?: string;
+    popover?: true | "" | "auto" | "manual" | "hint";
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'el-dropdown': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'el-menu': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      'el-disclosure': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
+  }
+}
+
