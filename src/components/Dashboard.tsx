beee65a799fb30ad215453c600c6e85ff6019d25
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-06 09:09:47 +0100

    Fix scrolling issue

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,7 +2,7 @@
 export const Dashboard = () => {
   return (
     <>
-      <header className="relative shadow-sm" id="dashboard">
+      <header className="relative shadow-sm scroll-mt-16" id="dashboard">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
             Dashboard
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -2,7 +2,7 @@
 export const Team = () => {
   return (
     <>
-      <header className="relative shadow-sm" id="team">
+      <header className="relative shadow-sm scroll-mt-16" id="team">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
             Team
