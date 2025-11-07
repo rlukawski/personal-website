cc4e89739f8d8289a7126b483ce87c1e85903d65
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 11:09:32 +0100

    Style navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 export const Navigation = () => {
   return (
-    <div className="w-full flex items-center justify-between px-12">
+    <div className="w-full flex items-center justify-between px-12 py-2 sticky top-0 z-10 bg-transparent backdrop-blur-sm shadow-sm">
       <p className="text-gray-800 text-3xl font-bold">Rafał Łukawski</p>
       <ul className="flex gap-6 text-lg">
         <li>Home</li>
