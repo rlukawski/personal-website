5001654808fc8f1c11f8b13844f2601a1ffd6164
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 06:09:32 +0100

    Update navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,15 @@
 export const Navigation = () => {
   return (
-    <div className="w-full flex px-8 border border-red-500">
-        <p className="text-gray-800 text-3xl font-bold">Rafał Łukawski</p>
+    <div className="w-full flex items-center justify-between px-12 border border-red-500">
+      <p className="text-gray-800 text-3xl font-bold">Rafał Łukawski</p>
+      <ul className="flex gap-6 text-lg">
+        <li>Home</li>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+      <p className="text-gray-800 text-xl font-bold">Contact</p>
     </div>
   );
 };
