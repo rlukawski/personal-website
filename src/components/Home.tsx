2c575aafa8116e8604a9c431d473ff182fd23b52
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 06:34:40 +0100

    Update header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,12 +3,12 @@ import authorImg from "../assets/author.jpg";
 export const Home = () => {
   return (
     <main id="home" className="mt-16">
-      <div className="flex w-full justify-around items-center border border-red-500">
+      <div className="flex w-full justify-evenly items-center">
         <div className="h-full flex flex-col gap-2">
-          <p className="header-text">A full stack developer and IT Project Manager</p>
+          <p className="text-gray-600 text-2xl font-bold">A full-stack developer and IT Project Manager</p>
           <div className="flex gap-2 items-center pl-4">
             <div className="w-2 h-2 rounded-full bg-green-500"></div>
-            <p className="body-text">Let’s build something great together!</p>
+            <p className="text-gray-600 text-lg font-bold">Let's build products that matter!</p>
           </div>
         </div>
         <div className="w-64">
