cdc37588e6ff679fb4677bace2be827758c63429
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 05:13:13 +0100

    Fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -153,8 +153,8 @@ export default function Example(): React.ReactElement {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 p-0 md:p-4">
-      <Disclosure as="nav" className="backdrop-blur-sm sticky top-0 z-50">
+    <div className="min-h-screen bg-gray-900 p-0 md:px-4">
+      <Disclosure as="nav" className="backdrop-blur-sm sticky top-4 z-50">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="flex items-center justify-center grow">
