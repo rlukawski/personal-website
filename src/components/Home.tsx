e903a29a95c98eff8e78b3bac27ac3c584d4cff5
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 06:30:05 +0100

    Update header

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,8 +4,12 @@ export const Home = () => {
   return (
     <main id="home" className="mt-16">
       <div className="flex w-full justify-around items-center border border-red-500">
-        <div className="h-full border border-blue-500 header-text">
-          <p>I create software that evolves with you</p>
+        <div className="h-full flex flex-col gap-2">
+          <p className="header-text">A full stack developer and IT Project Manager</p>
+          <div className="flex gap-2 items-center pl-4">
+            <div className="w-2 h-2 rounded-full bg-green-500"></div>
+            <p className="body-text">Let’s build something great together!</p>
+          </div>
         </div>
         <div className="w-64">
           <img src={authorImg} alt="Rafał Łukawski" className="rounded-xl" />
