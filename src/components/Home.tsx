debf59e8d2a512a68bfcf71a78933480a25bbe77
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 06:14:32 +0100

    Update navigation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,18 +2,15 @@ import authorImg from "../assets/author.jpg";
 
 export const Home = () => {
   return (
-    <>
-      <header id="home" className="header-box" />
-      <main>
-        <div className="flex w-full justify-around items-center border border-red-500">
-          <div className="h-full border border-blue-500 header-text">
-            <p>I create software that evolves with you</p>
-          </div>
-          <div className="w-64">
-            <img src={authorImg} alt="Rafał Łukawski" className="rounded-xl" />
-          </div>
+    <main id="home" className="mt-16">
+      <div className="flex w-full justify-around items-center border border-red-500">
+        <div className="h-full border border-blue-500 header-text">
+          <p>I create software that evolves with you</p>
         </div>
-      </main>
-    </>
+        <div className="w-64">
+          <img src={authorImg} alt="Rafał Łukawski" className="rounded-xl" />
+        </div>
+      </div>
+    </main>
   );
 };
