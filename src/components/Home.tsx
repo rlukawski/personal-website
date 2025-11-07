a5e1872cc0a811dacd7fb17b73cb3bd43d961fb8
Rafał Łukawski <rs.lukawski@gmail.com>
2025-11-07 05:44:50 +0100

    fix styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,18 @@
+import authorImg from "../assets/author.jpg";
+
 export const Home = () => {
   return (
     <>
       <header id="home" className="header-box" />
-      <main className="h-192 header-text">
-        <p>I create software that evolves with you</p>
+      <main className="h-192">
+        <div className="flex w-full justify-around items-center border border-red-500">
+          <div className="h-full border border-blue-500">
+            <p>I create software that evolves with you</p>
+          </div>
+          <div className="w-64">
+            <img src={authorImg} alt="Rafał Łukawski" />
+          </div>
+        </div>
       </main>
     </>
   );
